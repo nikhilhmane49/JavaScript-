35c7cda9a17fac0860f5a876ce084ced0de6d830
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-06-10 10:08:07 +0000

    container config done

---
+++ basic_01/variable.js
@@ -0,0 +1,26 @@
+
+//const---
+const accoutId=1253;
+
+//accoutId=24516;----> in this in can not change the value of the variable
+
+//variable----
+
+//1)let  ---> mostly used nowadays 
+//2)var ----> we dose not used this variable(because it dose not have any scope ) 
+
+
+let accoutEmail="hello@gmail.com";
+
+accountEmail="byee@gmail.com"; //--> in this we can change the value of the variable
+
+
+accout="hello you are you"; //---> in this the variable is assigned but it is bad to assign the variable
+
+
+console.log(accoutId);  //--->In this only single variable can print
+
+console.table([accoutId,accountEmail,accout]);  //---> in this more thane one variable can print, in the form of table
+
+
+
