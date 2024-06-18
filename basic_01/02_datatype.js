5103f1c22698e6e63917452727ce8d52979c6067
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-06-18 16:17:54 +0000

    datatype

---
+++ basic_01/02_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all the js code as newer version
+
+//alter(3+3) //we are using nodejs not in browser 
+
+//Datatype
+
+//premitive
+
+//1)number
+//2)bigint
+//3)string=>" "
+//4)boolean=>true/flase
+//5)null=>standalone value
+//6)undefined=>
+//7)symbol=>unique
+
+//non-premitive
+
+//1)object
+//2)array
+//3)function
+
+
+let age=10;
+
+let fristname="nikhil";
+
+//typeof
+console.log(typeof age); //number
+
+console.log(typeof fristname); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined 
