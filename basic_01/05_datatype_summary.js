ffc3481379c35ad7639a9c4bb0c8c0c6c47b56c9
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-06-20 06:21:04 +0000

    datatype summary (interview)

---
+++ basic_01/05_datatype_summary.js
@@ -0,0 +1,41 @@
+
+
+//premitive and non-premitive
+
+//############****premitive
+
+//---->the premitive and non-premitive datatype is divided on the base of 1)how the data is store in memory
+//                                                               2)And how it is acess
+// ---->non-premitive is also called as reference type
+
+//premitive----->>(call by value) //7 types
+
+//1)String--->" "
+//2)Number--->simple number
+//3)Boolearn--->true and false
+//4)null-->empty
+//5)undefined-->variable is declared and memory space is declared but not the value
+//6)symbol--->to make any value unique 
+//7)BigInt--->value that is biger 
+
+
+//########*********
+
+//javascript is a dynamic language.
+//Here are some reasons why:
+
+//1)Dynamic Typing: Variables in JavaScript can hold values of any data type, and the type can change at runtime.
+//   For example, you can assign a number to a variable and later assign a string to the same variable without any type errors.
+
+
+//2)Runtime Object Construction: You can add or remove properties 
+//  from objects at runtime. This means the structure of objects is not fixed and can be modified dynamically.
+
+//########*****non-premitive
+
+
+//non-premitive---//(oR reference type)
+
+//1)array
+//2)Object
+//3)function
