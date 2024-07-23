dbad984a19ef78a18e649724cb29e928b7b7e8c8
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-07-23 16:38:28 +0000

    Javasprict execucation

---
+++ JavaScript_execute/01_execucation.js
@@ -0,0 +1,15 @@
+
+
+//JavaScript Execution Context------->Hwo the  code is executing javasprict
+
+
+
+// when we give the code to javasript ------frist----Global Execution Context is created ---and it is stored in This variable
+
+
+//In Brower ---The value of (this)---is Window Object
+
+
+//javasript is single threaded language 
+
+
