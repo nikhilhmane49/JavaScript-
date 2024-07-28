8485006d594f1a13b533a8fcb7ea9d9af219b7b3
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-07-28 20:41:12 +0000

    Truthy_falsy_value

--- basic_04/03_truthy_falsy.js
+++ basic_04/03_truthy_falsy.js
@@ -26,9 +26,31 @@
 
 //Hwo to ckech the array and object is empty or not
 
-
+//**array */
 const user=[];
 
 if(user.length === 0){
     console.log("the array is empty");
+}
+
+//****Object */
+
+const user2={};
+
+if(Object.keys(user2).length ===0){    //Object.keys(user2)--->This will give me a array
+    console.log("the Object is empty");
+}
+
+//******* */
+
+if(false==0){
+    console.log("execute"); //output--->execute
+}
+
+if(false==""){
+    console.log("execute"); //output--->execute
+}
+
+if(0 == ""){
+    console.log("execute"); //output--->execute
 }
