5dbae55a91d3b37f03dd99a0b6c70fd1385f40e4
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-07-30 21:29:41 +0000

    While_loop and break_continue

---
+++ basic_04/06_While_loop.js
@@ -0,0 +1,40 @@
+//While Loop
+
+let index=0;
+
+while (index<=10) {
+    console.log(`value of the index ${index}`);
+    index++;
+}
+
+
+let myarray=[1,2,3,4,5];
+
+let arr=0;
+
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr++;
+}
+
+
+
+
+//Do While loop
+
+let score=1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<10);
+
+
+
+let score2=11;
+
+do {
+    console.log(`Score is ${score2}`); //Output---->Score is 11
+    score2++;
+} while (score2<10);
+
