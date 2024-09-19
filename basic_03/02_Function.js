ded1c92b5fc2c406a364fb1de4a017e3f89d24f1
Nikhil Hambirrao Mane <137916068+nikhilhmane49@users.noreply.github.com>
2024-09-19 22:05:43 +0530

    Update 02_Function.js

--- basic_03/02_Function.js
+++ basic_03/02_Function.js
@@ -41,4 +41,38 @@ function returnSecoundValue(getarray) {
 
 console.log(returnSecoundValue(myarray));
 
+
+
+
+//**********call back-function
+
+
+
+
+
+function sum(num1 , num2 , call){
+    
+    let total=num1+num2;
+    
+    call(total);
+    
+}
+
+function Display1(Data){
+    
+    console.log("The sum of the is :"+ Data);
+    
+}
+
+function Display2(Data){
+    
+    console.log("The sum of the number is :"+Data);
+}
+
+
+//In this by calling Single Function we can call another function in it
+//Means in single call we can call Another function---->CallBack function
+
+sum(1 , 2 , Display2);
+
  
